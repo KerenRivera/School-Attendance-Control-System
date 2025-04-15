06bdd8d3258f4da860114072128a8ae999ab84ab
Keren <131375714+KerenRivera@users.noreply.github.com>
2025-04-14 22:21:05 -0400

    Updating the menu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-﻿Console.WriteLine("");
+﻿using System;
 
-Console.WriteLine("¡Bienvenido al Sistema de Asistencia!");
+namespace Sistema_de_Gestion_de_asistencias
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenido al Sistema de Asistencia!\n");
 
 Dictionary<int, string> names = new Dictionary<int, string>();
 Dictionary<int, string> phones = new Dictionary<int, string>();
@@ -11,14 +17,15 @@ List<int> ids = new List<int>();
 bool running = true;
 while (running)
 {
+                Console.WriteLine("¿Que necesita realizar?\n");
 
-    Console.Write("---------------MENU PRINCIPAL---------------  ");
-    Console.Write("1. Mostrar maestros");
-    Console.Write("2. Mostrar alumnos");
-    Console.Write("3. Mostrar cursos");
-    Console.Write("4. Registrar asistencia");
-    Console.Write("5. Mostrar reportes");
-    Console.Write("6. Salir");
+                Console.Write("---------------MENU PRINCIPAL--------------- \n");
+                Console.Write("1. Mostrar maestros\n");
+                Console.Write("2. Mostrar alumnos\n");
+                Console.Write("3. Mostrar cursos\n");
+                Console.Write("4. Registrar asistencia\n");
+                Console.Write("5. Mostrar reportes\n");
+                Console.Write("6. Salir\n");
     Console.Write("Elige una opción: ");
 
     int choice = Convert.ToInt32(Console.ReadLine());
@@ -50,9 +57,6 @@ while (running)
     }
 }
 
-static void AddContact(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
-{
-    Console.WriteLine("Vamos a agregar ese contacte que te trae loco.");
 
     int id = ids.Count() + 1;
     ids.Add(id);
@@ -94,49 +98,68 @@ static void EditContact(ref Dictionary<int, string> names, ref Dictionary<int, s
     var emailSeleccionado = emails[idSeleccionado];
     string direccionSeleccionada = addresses[idSeleccionado - 1];
 
-    Console.Write($"El nombre es: {nombreSeleccionado}, Digite el Nuevo Nombre: ");
-    var name = Console.ReadLine();
-    names.Remove(idSeleccionado);
-    names.Add(idSeleccionado, name);
+//static void ViewAttendance(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
+//{
+//    Console.WriteLine("Id           Nombre          Telefono            Email           Dirección");
+//    Console.WriteLine("___________________________________________________________________________");
 
-    Console.Write($"El Teléfono es: {telefonoSeleccionado}, Digite el Nuevo Teléfono: ");
-    var telefono = Console.ReadLine();
-    phones.Remove(idSeleccionado);
-    phones.Add(idSeleccionado, telefono);
+//    foreach (var id in ids)
+//    {
+//        Console.WriteLine($"{id}    {names[id]}      {phones[id]}      {emails[id]}     {addresses[id - 1]}");
+//    }
+//}
 
-    Console.Write($"El Email es: {emailSeleccionado}, Digite el Nuevo Email: ");
-    var email = Console.ReadLine();
-    emails.Remove(idSeleccionado);
-    emails.Add(idSeleccionado, email);
+//static void EditAttendance(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
+//{
+//    ViewContacts(ref names, ref phones, ref emails, ref addresses, ref ids);
+//    Console.WriteLine("Digite un  Id de Contacto Para Editar");
+//    int idSeleccionado = Convert.ToInt32(Console.ReadLine());
+//    var nombreSeleccionado = names[idSeleccionado];
+//    var telefonoSeleccionado = phones[idSeleccionado];
+//    var emailSeleccionado = emails[idSeleccionado];
+//    string direccionSeleccionada = addresses[idSeleccionado - 1];
 
-    Console.Write($"La dirección es: {direccionSeleccionada}, Digite la nueva dirección: ");
-    var address = Console.ReadLine();
-    addresses.RemoveAt(idSeleccionado - 1);
-    addresses.Add(address);
-}
+//    Console.Write($"El nombre es: {nombreSeleccionado}, Digite el Nuevo Nombre: ");
+//    var name = Console.ReadLine();
+//    names.Remove(idSeleccionado);
+//    names.Add(idSeleccionado, name);
 
-static void DeleteContact(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
-{
-    ViewContacts(ref names, ref phones, ref emails, ref addresses, ref ids);
-    Console.WriteLine("Digite un Id de Contacto Para Eliminar");
-    int idSeleccionado = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Seguro que desea eliminar? 1. Si, 2. No");
-    int opcion = Convert.ToInt32(Console.ReadLine());
-    if (opcion == 1)
-    {
-        var nombreSeleccionado = names[idSeleccionado];
-        var telefonoSeleccionado = phones[idSeleccionado];
-        var emailSeleccionado = emails[idSeleccionado];
-        string direccionSeleccionada = addresses[idSeleccionado - 1];
+//    Console.Write($"El Teléfono es: {telefonoSeleccionado}, Digite el Nuevo Teléfono: ");
+//    var telefono = Console.ReadLine();
+//    phones.Remove(idSeleccionado);
+//    phones.Add(idSeleccionado, telefono);
 
-        names.Remove(idSeleccionado);
-        phones.Remove(idSeleccionado);
-        emails.Remove(idSeleccionado);
-        addresses.RemoveAt(idSeleccionado - 1);
-        ids.RemoveAt(idSeleccionado - 1);
-    }
+//    Console.Write($"El Email es: {emailSeleccionado}, Digite el Nuevo Email: ");
+//    var email = Console.ReadLine();
+//    emails.Remove(idSeleccionado);
+//    emails.Add(idSeleccionado, email);
 
-}
+//    Console.Write($"La dirección es: {direccionSeleccionada}, Digite la nueva dirección: ");
+//    var address = Console.ReadLine();
+//    addresses.RemoveAt(idSeleccionado - 1);
+//    addresses.Add(address);
+//}
+
+//static void DeleteAttendance(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
+//{
+//    ViewContacts(ref names, ref phones, ref emails, ref addresses, ref ids);
+//    Console.WriteLine("Digite un Id de Contacto Para Eliminar");
+//    int idSeleccionado = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine("Seguro que desea eliminar? 1. Si, 2. No");
+//    int opcion = Convert.ToInt32(Console.ReadLine());
+//    if (opcion == 1)
+//    {
+//        var nombreSeleccionado = names[idSeleccionado];
+//        var telefonoSeleccionado = phones[idSeleccionado];
+//        var emailSeleccionado = emails[idSeleccionado];
+//        string direccionSeleccionada = addresses[idSeleccionado - 1];
+
+//        names.Remove(idSeleccionado);
+//        phones.Remove(idSeleccionado);
+//        emails.Remove(idSeleccionado);
+//        addresses.RemoveAt(idSeleccionado - 1);
+//        ids.RemoveAt(idSeleccionado - 1);
+//    }
 
 static void SearchContact(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
 {
