e5104084fc7fa6e52d80aa16ba53043e25dcfe24
Keren <131375714+KerenRivera@users.noreply.github.com>
2025-04-17 16:06:54 -0400

    Updating the menu(again) and submenu
    I realized i did the menu wrong, it wasn't actually a CRUD cause it didn't let the user to update the students, professors or classes but just to see them

---
+++ Helpers/Studenthelper.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Gestion_de_asistencias.Helpers
+{
+	public class StudentHelper
+	{
+		public static void ShowSubmenu()
+		{
+			Console.WriteLine("1. Agregar Alumno\n");
+			Console.WriteLine("2. Editar Alumno\n");
+			Console.WriteLine("3. Eliminar Alumno\n");
+			Console.WriteLine("4. Atrás\n")
+			var option = Console.ReadLine();
+
+			switch (option)
+			{
+				case 1:
+					AddStudent();
+					break;
+				case 2:
+					EditStudent();
+					break;
+				case 3:
+					DeleteStudent();
+					break;
+				case 4:
+					//
+			}
+
+			//List<Alumno> alumnos = new List<Alumno>();
+		public static void AddStudent() 
+		{
+			Console.Write("Ingrese el nombre del alumno: ");
+			string nombre = Console.ReadLine();
+			Console.Write("Ingrese el apellido del alumno: ");
+			string apellido = Console.ReadLine();
+			Console.Write("Ingrese el curso asignado: ");
+			string Curso = Console.ReadLine();
+
+			using (var context = new DbContext())
+			{
+				var alumno = new Alumno
+				{
+					Nombre = nombre,
+					Apellido = apellido,
+					Curso = Curso
+				};
+
+				context.Alumnos.Add(alumno);
+				context.SaveChanges();
+				Console.WriteLine("Alumno agregado exitosamente.");
+			}
+
+		}
+
+		public static void EditStudent()
+		{
+			Console.Write("Ingrese la matricula del alumno a editar: ");
+			int matricula = int.Parse(Console.ReadLine());
+
+			using (var context = new DbContext())
+			{
+				var alumno = context.Alumnos.Include(a => a.Curso).FirstOrDefault(a => a.Matricula == matricula);
+
+				if (alumno == null)
+				{
+					Console.WriteLine("Alumno no encontrado.");
+					return;
+				}
+				else
+				{
+					Console.WriteLine($"Alumno encontrado: {alumno.Nombre} {alumno.Apellido}");
+
+					Console.Write("Nuevo nombre del alumno(Dejar vacio si no desea cambiarlo): ");
+					string nuevoNombre = Console.ReadLine();
+					if (!string.IsNullOrEmpty(nuevoNombre))
+					{
+						alumno.Nombre = nuevoNombre;
+					}
+					Console.Write("Nuevo apellido del alumno(Dejar vacio si no desea cambiarlo): ");
+					string nuevoApellido = Console.ReadLine();
+					if (!string.IsNullOrEmpty(nuevoApellido))
+					{
+						alumno.Apellido = nuevoApellido;
+					}
+					Console.Write("Nuevo ID de curso asignado(Dejar vacio si no desea cambiarlo): ");
+					string nuevoIDCurso = Console.ReadLine();
+					if (!string.IsNullOrEmpty(nuevoIDCurso))
+					{
+						alumno.IdCurso = int.Parse(nuevoIDCurso);
+					}
+					context.SaveChanges();
+					Console.WriteLine("Alumno editado exitosamente.");
+				}
+			}
+
+		}
+
+		public static void DeleteStudent()
+		{
+			//jjbjhbjbjbiub
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,51 +1,46 @@
-﻿using System;
+﻿//using Sistema_de_Gestion_de_asistencias.Helpers;
 
-namespace Sistema_de_Gestion_de_asistencias
+namespace Sistema_de_Gestion_de_asistencias.Helpers
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            //var context = new Persistence.DataContext();
             Console.WriteLine("¡Bienvenido al Sistema de Asistencia!\n");
-
-            //Dictionary<int, string> names = new Dictionary<int, string>();
-            //Dictionary<int, string> phones = new Dictionary<int, string>();
-            //Dictionary<int, string> emails = new Dictionary<int, string>();
-            //List<string> addresses = new List<string>();
-            //List<int> ids = new List<int>();
 
             bool running = true;
             while (running)
             {
-                Console.WriteLine("¿Que necesita realizar?\n");
+                Console.WriteLine("¿Qué necesita realizar?\n");
 
                 Console.Write("---------------MENU PRINCIPAL--------------- \n");
-                Console.Write("1. Mostrar maestros\n");
-                Console.Write("2. Mostrar alumnos\n");
-                Console.Write("3. Mostrar cursos\n");
+                Console.Write("1. Gestionar estudiantes\n");
+                Console.Write("2. Gestionar maestros\n");
+                Console.Write("3. Gestionar cursos\n");
                 Console.Write("4. Registrar asistencia\n");
-                Console.Write("5. Mostrar reportes\n");
+                Console.Write("5. Mostrar reporte de Asistencia semanal\n");
                 Console.Write("6. Salir\n");
                 Console.Write("Elige una opción: ");
-
                 int choice = Convert.ToInt32(Console.ReadLine());
+
 
                 switch (choice)
                 {
                     case 1:
-                        // AddContact(ref names, ref phones, ref emails, ref addresses, ref ids);
+                        Studenthelper.ShowSubmenu();
                         break;
                     case 2:
-                        //ViewContacts(ref names, ref phones, ref emails, ref addresses, ref ids);
+                        
                         break;
                     case 3:
-                        //EditContact(ref names, ref phones, ref emails, ref addresses, ref ids);
+                        
                         break;
                     case 4:
-                        //DeleteContact(ref names, ref phones, ref emails, ref addresses, ref ids);
+                        
                         break;
                     case 5:
-                        //SearchContact(ref names, ref phones, ref emails, ref addresses, ref ids);
+                        
                         break;
                     case 6:
                         running = false;
@@ -57,124 +52,6 @@ namespace Sistema_de_Gestion_de_asistencias
                 }
             }
 
-
-            //    int id = ids.Count() + 1;
-            //    ids.Add(id);
-            //    Console.Write("Digite el Nombre: ");
-            //    var name = Console.ReadLine();
-            //    names.Add(id, name);
-            //    Console.Write("Digite el Teléfono: ");
-            //    var telefono = Console.ReadLine();
-            //    phones.Add(id, telefono);
-
-            //    Console.Write("Digite el Email: ");
-            //    var email = Console.ReadLine();
-            //    emails.Add(id, email);
-            //    Console.Write("Digite la dirección: ");
-            //    var address = Console.ReadLine();
-            //    addresses.Add(address);
-            //    Console.WriteLine();
-
-            //}
-
-            //static void ViewContacts(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
-            //{
-            //    Console.WriteLine("Id           Nombre          Telefono            Email           Dirección");
-            //    Console.WriteLine("___________________________________________________________________________");
-
-            //    foreach (var id in ids)
-            //    {
-            //        Console.WriteLine($"{id}    {names[id]}      {phones[id]}      {emails[id]}     {addresses[id - 1]}");
-            //    }
-            //}
-
-            //static void EditContact(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
-            //{
-            //    ViewContacts(ref names, ref phones, ref emails, ref addresses, ref ids);
-            //    Console.WriteLine("Digite un  Id de Contacto Para Editar");
-            //    int idSeleccionado = Convert.ToInt32(Console.ReadLine());
-            //    var nombreSeleccionado = names[idSeleccionado];
-            //    var telefonoSeleccionado = phones[idSeleccionado];
-            //    var emailSeleccionado = emails[idSeleccionado];
-            //    string direccionSeleccionada = addresses[idSeleccionado - 1];
-
-            //static void ViewAttendance(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
-            //{
-            //    Console.WriteLine("Id           Nombre          Telefono            Email           Dirección");
-            //    Console.WriteLine("___________________________________________________________________________");
-
-            //    foreach (var id in ids)
-            //    {
-            //        Console.WriteLine($"{id}    {names[id]}      {phones[id]}      {emails[id]}     {addresses[id - 1]}");
-            //    }
-            //}
-
-            //static void EditAttendance(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
-            //{
-            //    ViewContacts(ref names, ref phones, ref emails, ref addresses, ref ids);
-            //    Console.WriteLine("Digite un  Id de Contacto Para Editar");
-            //    int idSeleccionado = Convert.ToInt32(Console.ReadLine());
-            //    var nombreSeleccionado = names[idSeleccionado];
-            //    var telefonoSeleccionado = phones[idSeleccionado];
-            //    var emailSeleccionado = emails[idSeleccionado];
-            //    string direccionSeleccionada = addresses[idSeleccionado - 1];
-
-            //    Console.Write($"El nombre es: {nombreSeleccionado}, Digite el Nuevo Nombre: ");
-            //    var name = Console.ReadLine();
-            //    names.Remove(idSeleccionado);
-            //    names.Add(idSeleccionado, name);
-
-            //    Console.Write($"El Teléfono es: {telefonoSeleccionado}, Digite el Nuevo Teléfono: ");
-            //    var telefono = Console.ReadLine();
-            //    phones.Remove(idSeleccionado);
-            //    phones.Add(idSeleccionado, telefono);
-
-            //    Console.Write($"El Email es: {emailSeleccionado}, Digite el Nuevo Email: ");
-            //    var email = Console.ReadLine();
-            //    emails.Remove(idSeleccionado);
-            //    emails.Add(idSeleccionado, email);
-
-            //    Console.Write($"La dirección es: {direccionSeleccionada}, Digite la nueva dirección: ");
-            //    var address = Console.ReadLine();
-            //    addresses.RemoveAt(idSeleccionado - 1);
-            //    addresses.Add(address);
-            //}
-
-            //static void DeleteAttendance(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
-            //{
-            //    ViewContacts(ref names, ref phones, ref emails, ref addresses, ref ids);
-            //    Console.WriteLine("Digite un Id de Contacto Para Eliminar");
-            //    int idSeleccionado = Convert.ToInt32(Console.ReadLine());
-            //    Console.WriteLine("Seguro que desea eliminar? 1. Si, 2. No");
-            //    int opcion = Convert.ToInt32(Console.ReadLine());
-            //    if (opcion == 1)
-            //    {
-            //        var nombreSeleccionado = names[idSeleccionado];
-            //        var telefonoSeleccionado = phones[idSeleccionado];
-            //        var emailSeleccionado = emails[idSeleccionado];
-            //        string direccionSeleccionada = addresses[idSeleccionado - 1];
-
-            //        names.Remove(idSeleccionado);
-            //        phones.Remove(idSeleccionado);
-            //        emails.Remove(idSeleccionado);
-            //        addresses.RemoveAt(idSeleccionado - 1);
-            //        ids.RemoveAt(idSeleccionado - 1);
-            //    }
-
-            //static void SearchContact(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
-            //{
-            //ViewContacts(ref names, ref phones, ref emails, ref addresses, ref ids);
-            //Console.WriteLine("Digite un Id de Contacto Para Mostrar");
-            //int idSeleccionado = Convert.ToInt32(Console.ReadLine());
-            //var nombreSeleccionado = names[idSeleccionado];
-            //var telefonoSeleccionado = phones[idSeleccionado];
-            //var emailSeleccionado = emails[idSeleccionado];
-            //string direccionSeleccionada = addresses[idSeleccionado - 1];
-
-            //Console.Write($"El nombre es: {nombreSeleccionado}");
-            //Console.Write($"El Teléfono es: {telefonoSeleccionado}");
-            //Console.Write($"El Email es: {emailSeleccionado}");
-            //Console.Write($"La dirección es: {direccionSeleccionada}");
         }
     }
 
