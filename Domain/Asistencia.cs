044057425d45266815a7317593465e2ea36155da
Keren <131375714+KerenRivera@users.noreply.github.com>
2025-04-25 21:17:30 -0400

    Update Asistencia.cs

--- Domain/Asistencia.cs
+++ Domain/Asistencia.cs
@@ -22,10 +22,13 @@ namespace Sistema_de_Gestion_de_asistencias.Domain
         public int IdAsistencia { get; set; }
         public int IdPersona { get; set; }
         public int IdCurso { get; set; }
+        public int IdClase { get; set; }
         [ForeignKey("IdPersona")]
         public required Alumno Alumno { get; set; }
         [ForeignKey("IdCurso")]
         public required Curso Curso { get; set; }
+        [ForeignKey("IdClase")]
+        public Clase Clase { get; set; } = null!;
         [Column(TypeName = "char(1)")]
         public EstadoAsistencia Estado { get; set; }
         public DateTime Fecha { get; set; }
